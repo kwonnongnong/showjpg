b570bd3a978850a0a798e4568e23d6366059c0ad
kwonnongnong <kwonnongnong@gmail.com>
2015-02-22 22:21:57 +0900

    최초코

---
+++ ShowJPG/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace ShowJPG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(170, 16);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "JPG1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(170, 52);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 29);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "JPG2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(170, 89);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 29);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "JPG3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 136);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(354, 188);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.Location = new System.Drawing.Point(376, 20);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(619, 574);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(32, 15);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(86, 29);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Start";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button4_MouseDown);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(32, 74);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(86, 29);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Quit";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button5_MouseDown);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(18, 388);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(170, 388);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(100, 23);
+            this.progressBar2.TabIndex = 8;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM3";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(262, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 625);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ ShowJPG/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.IO;
+using System.IO.Ports;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace ShowJPG
+{
+    public partial class Form1 : Form
+    {
+        int Rwidth;
+        int Rheight;
+        int Pwidth;
+        int Pheight;
+        Rectangle[] Rect = new Rectangle[3];
+        string strDir = "..\\..\\..\\..\\JPG_files\\";
+        string[] fnames = { "S1.jpg", "S2.jpg", "S3.jpg" };
+        Bitmap PhotoImage;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            int i, n=0;
+            Graphics formGraphics = pictureBox1.CreateGraphics();
+            for(i=0; i<3; i++) formGraphics.FillRectangle(Brushes.Black, Rect[i]);
+           // serialPort1;
+
+            if (sender == this.button1) { progressBar1.Value = progressBar1.Value + 10; }
+            else if (sender == this.button2) { progressBar1.Value = progressBar1.Value + 10; progressBar2.Value = progressBar2.Value + 10; }
+            else if (sender == this.button3) { progressBar2.Value = progressBar2.Value + 10; }
+            else return;
+            
+            
+            if (progressBar1.Value == 100 && progressBar2.Value == 100)
+            {
+                progressBar1.Value = 0;
+                progressBar2.Value = 0;
+                n = 1;
+            }
+
+            else if (progressBar1.Value == 100){
+                progressBar1.Value = 0;
+                progressBar2.Value = 0;
+                n = 0;
+            }
+            else if (progressBar2.Value == 100) {
+                progressBar1.Value = 0;
+                progressBar2.Value = 0;
+                n = 2;
+            }
+
+            formGraphics.FillRectangle(Brushes.Red, Rect[n]);
+            DownloadRemoteImageFile("https://z.enha.kr/http://rigvedawiki.net/r1/pds/_ec_95_84_ec_9d_b4_ec_82_ac_ec_b9_b4_20_ed_83_80_ec_9d_b4_ea_b0_80/aisaka_taiga.jpg");
+            showPhoto(PhotoImage);
+            formGraphics.Dispose();
+        }
+
+        private void DrawGrid()
+        {
+            Rwidth = pictureBox1.ClientSize.Width;
+            Rheight = pictureBox1.ClientSize.Height;
+
+            Pwidth = pictureBox2.ClientSize.Width;
+            Pheight = pictureBox2.ClientSize.Height;
+
+            double pi = Math.PI;
+            int x = Rheight - (int)(Rheight * Math.Cos(pi * 50 / 180));
+            int y = Rheight - (int)(Rheight * Math.Sin(pi * 30 / 180))+20;
+            Rect[0] = new Rectangle(x, y, 20, 20);
+            Rect[1] = new Rectangle(Rwidth / 2-10, Rheight/3, 20, 20);
+            Rect[2] = new Rectangle(Rwidth - x-20, y, 20, 20);
+
+
+            Graphics formGraphics = pictureBox1.CreateGraphics();
+            formGraphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            formGraphics.Clear(Color.Black);
+
+            Pen pen1 = new Pen(Color.FromArgb(255, 0, 100, 0),2);
+            Pen pen2 = new Pen(Color.FromArgb(255, 0, 220, 100),2);
+
+            formGraphics.DrawPie(pen1, 0, 0, 2 * Rheight, 2 * Rheight, 180, 130);
+            formGraphics.DrawPie(pen2, Rwidth - 2 * Rheight, 0, 2 * Rheight, 2 * Rheight, 230, 130);
+
+            pen1.Dispose();
+            pen2.Dispose();
+            formGraphics.Dispose();
+        }//감지부분 출력부
+
+        private void button4_MouseDown(object sender, MouseEventArgs e)
+        {
+            DrawGrid();
+            serialPort1.Open();
+            if (serialPort1.IsOpen)
+            {
+                Console.Write("포트연결성공");
+            }
+        }
+
+        private void button5_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.Dispose();
+        }
+        private void showPhoto(Bitmap inputBitmap)
+        {
+            //string fname = strDir + fnames[n];
+            try
+            {
+               // PhotoImage = new Bitmap(inputstream);
+                pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox2.Image = (Image)PhotoImage;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error!! : filename error");
+            }
+        }//image 출력부
+
+
+        private void DownloadRemoteImageFile(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                bool bImage = response.ContentType.StartsWith("image",
+                StringComparison.OrdinalIgnoreCase);//request된 패킷의 contenType이 image 형태인지 확인
+                if ((response.StatusCode == HttpStatusCode.OK ||
+                    response.StatusCode == HttpStatusCode.Moved ||
+                    response.StatusCode == HttpStatusCode.Redirect) &&
+                    bImage)
+                {
+                    using (Stream inputStream = response.GetResponseStream())//respon된 stream INputStream에 삽입
+                    PhotoImage = new Bitmap(inputStream);
+                   // return true;
+                }
+                else
+                {
+                   // return false;
+                }
+            }
+            catch (WebException e)
+            {
+                Console.Write("에러 메시지" + e.Message);
+               // return false;
+            }
+        }
+        //**********************************************************************//
+        //ipcamera 이미지 획득부//
+        /*
+        private static bool DownloadRemoteImageFile(string url, string fileName)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                bool bImage = response.ContentType.StartsWith("image",
+                StringComparison.OrdinalIgnoreCase);//request된 패킷의 contenType이 image 형태인지 확인
+                if ((response.StatusCode == HttpStatusCode.OK ||
+                    response.StatusCode == HttpStatusCode.Moved ||
+                    response.StatusCode == HttpStatusCode.Redirect) &&
+                    bImage)
+                {
+                    using (Stream inputStream = response.GetResponseStream())//respon된 stream INputStream에 삽입
+                    using (Stream inputStream2 = response.GetResponseStream())//respon된 stream INputStream에 삽입
+                    
+                       
+                    using (Stream outputStream = File.OpenWrite(fileName))//outputStream은 fileopen과 연결
+                    {
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+                        do
+                        {
+
+                            bytesRead = inputStream.Read(buffer, 0, buffer.Length);//input stream의 크기를 알아내서
+                            outputStream.Write(buffer, 0, bytesRead);//파일로 쓰기
+                        } while (bytesRead != 0);
+                    }
+
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (WebException e)
+            {
+                Console.Write("에러 메시지" + e.Message);
+                return false;
+            }
+        }*/
+        //**********************************************************************//
+        //시리얼 입력시 작동부분//
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            int i, n = 0;
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadLine();
+            Graphics formGraphics = pictureBox1.CreateGraphics();
+            for (i = 0; i < 3; i++) formGraphics.FillRectangle(Brushes.Black, Rect[i]);
+            // serialPort1;
+            if (indata == "100\r") progressBar1.Value = progressBar1.Value + 10;
+            else if (indata == "010\r") { progressBar1.Value = progressBar1.Value + 10; progressBar2.Value = progressBar2.Value + 10; }
+            else if (indata == "001\r") { progressBar2.Value = progressBar2.Value + 10; }
+            else Console.WriteLine(indata);
+
+
+            if (progressBar1.Value == 100 && progressBar2.Value == 100)
+            {
+                progressBar1.Value = 0;
+                progressBar2.Value = 0;
+                n = 1;
+            }
+
+            else if (progressBar1.Value == 100)
+            {
+                progressBar1.Value = 0;
+                progressBar2.Value = 0;
+                n = 0;
+            }
+            else if (progressBar2.Value == 100)
+            {
+                progressBar1.Value = 0;
+                progressBar2.Value = 0;
+                n = 2;
+            }
+
+            formGraphics.FillRectangle(Brushes.Red, Rect[n]);
+            DownloadRemoteImageFile("https://z.enha.kr/http://rigvedawiki.net/r1/pds/_ec_95_84_ec_9d_b4_ec_82_ac_ec_b9_b4_20_ed_83_80_ec_9d_b4_ea_b0_80/aisaka_taiga.jpg");
+            showPhoto(PhotoImage);
+            formGraphics.Dispose();
+        }   
+       //****//
+    }
+}
