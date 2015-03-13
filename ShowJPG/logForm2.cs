1e0097232e96deab07885a5a01903e5c66e8fd80
kwonnongnong <kwonnongnong@gmail.com>
2015-03-14 00:09:02 +0900

    sql 쿼리 추

--- ShowJPG/logForm2.cs
+++ ShowJPG/logForm2.cs
@@ -14,6 +14,8 @@ namespace ShowJPG
     {
         Form1 mainForm;
         SQLiteConnection sqlconn;
+        int sql_offset=0;//초기 offset 0 ;
+        const int offset_size = 10;
         string connStr = @"Data Source=.\mydb.db";
         public logForm2()
         {
@@ -39,7 +41,7 @@ namespace ShowJPG
         {
             SQLiteConnection conn = Sqlconnect();
             DataSet ds = new DataSet();
-            string sql = "select * from log";
+            string sql = "select * from log limit 10 offset " + offset_size;
             var adpt = new SQLiteDataAdapter(sql, conn);
             adpt.Fill(ds);
 
@@ -48,24 +50,42 @@ namespace ShowJPG
         }
 
         private void button1_MouseDown(object sender, MouseEventArgs e)
-        {
+        {   /****tempcode**절대수정할것!!***/
             Console.WriteLine("클릭됨");
+            if (sql_offset > 0)
+                sql_offset = sql_offset - offset_size;
+            else
+                Console.WriteLine("가장 최근 Log입니다");
             DataSet ds = select_Adapter();
             DataTable table = ds.Tables["log"];
             foreach (DataRow row in table.Rows)//exception 발생함 수정필요
+            {
+                Console.WriteLine(row["No"].ToString() + row["F_name"].ToString() + row["date"].ToString());
+            }
+            /******************/
+            
+        }
 
+        private void button2_MouseDown(object sender, MouseEventArgs e)
+        {
+            sql_offset = 0;
+            DataSet ds = select_Adapter();
+            DataTable table = ds.Tables["log"];
+            foreach (DataRow row in table.Rows)//exception 발생함 수정필요
             {
                 Console.WriteLine(row["No"].ToString() + row["F_name"].ToString() + row["date"].ToString());
             }
         }
 
-        private void button2_MouseDown(object sender, MouseEventArgs e)
-        {
-
-        }
-
         private void button3_MouseDown(object sender, MouseEventArgs e)
         {
+            sql_offset = sql_offset + offset_size;
+            DataSet ds = select_Adapter();
+            DataTable table = ds.Tables["log"];
+            foreach (DataRow row in table.Rows)//exception 발생함 수정필요
+            {
+                Console.WriteLine(row["No"].ToString() + row["F_name"].ToString() + row["date"].ToString());
+            }
 
         }
     }
